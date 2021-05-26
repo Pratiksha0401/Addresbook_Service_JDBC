61312a217d8c5db9be8510519f87070790b09202
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-26 16:28:15 +0530

    added: code to insert new record to addressbook database

--- src/main/java/book/Addressbook_Service_WithNameandType/AddressbookRepo.java
+++ src/main/java/book/Addressbook_Service_WithNameandType/AddressbookRepo.java
@@ -35,6 +35,9 @@ public class AddressbookRepo {
 				while(result.next()) {
 					Contact info = new Contact();
 					
+					int id=result.getInt(1);
+					info.setId(id);
+					
 					String firstName = result.getString(2);
 					info.setFirstName(firstName);
 					
@@ -47,8 +50,20 @@ public class AddressbookRepo {
 					String city = result.getString(5);
 					info.setCity(city);
 					
+					String state = result.getString(6);
+					info.setState(state);
+					
+					String zip = result.getString(7);
+					info.setZipCode(zip);
+					
 					String phoneNo = result.getString(8);
 					info.setPhoneNo(phoneNo);
+					
+					String email = result.getString(9);
+					info.setEmail_ID(email);
+					
+					String date=result.getString(12);
+					info.setDate(date);
 					
 					details.add(info);
 				}
@@ -128,8 +143,20 @@ public class AddressbookRepo {
 					String city = result.getString(5);
 					info.setCity(city);
 					
+					String state = result.getString(6);
+					info.setState(state);
+					
+					String zip = result.getString(7);
+					info.setZipCode(zip);
+					
 					String phoneNo = result.getString(8);
 					info.setPhoneNo(phoneNo);
+					
+					String email = result.getString(9);
+					info.setEmail_ID(email);
+					
+					String date=result.getString(12);
+					info.setDate(date);
 					
 					details.add(info);
 				}
@@ -189,8 +216,20 @@ public class AddressbookRepo {
 					String city = result.getString(5);
 					info.setCity(city);
 					
+					String state = result.getString(6);
+					info.setState(state);
+					
+					String zip = result.getString(7);
+					info.setZipCode(zip);
+					
 					String phoneNo = result.getString(8);
 					info.setPhoneNo(phoneNo);
+					
+					String email = result.getString(9);
+					info.setEmail_ID(email);
+					
+					String date=result.getString(12);
+					info.setDate(date);
 					
 					details.add(info);
 				}
@@ -209,4 +248,40 @@ public class AddressbookRepo {
 				}
 				return details;
 		}
+		
+		public void insertRecord(Contact info) throws SQLException {
+			
+			Connection connection = null;
+			Statement statement = null;
+			try {
+			//Step1: Load & Register Driver Class
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
+			
+			//Step2: Establish a MySql Connection
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook_service", "root", "root");
+			
+			//Step3: Create Statement
+			 statement = connection.createStatement();
+			
+			//Step4: Execute Query
+			String query = "insert into addressBook(FirstName, LastName, Address, City, State, Zip, PhoneNo, Email_ID, addDate) "
+					        + "value('"+info.getFirstName()+"','"+info.getLastName()+"','"+info.getAddress()+"','"+info.getCity()+"','"+info.getState()+"','"+info.getZipCode()+"','"+info.getPhoneNo()+ "','"+info.getEmail_ID()+ "','"+info.getDate()+"')";
+			int result = statement.executeUpdate(query);
+			System.out.print("Rows affected : "+result);
+			
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(connection != null) {
+				statement.close();
+				}
+				if(statement != null) {
+				connection.close();
+				}
+			}
+			
+		}
+
 }
